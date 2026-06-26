62df8ed5d60076557b01b7ca42cff1b746171692
yalexx <yalexx@users.noreply.github.com>
2026-06-26 06:56:20 +0000

    deploy: d7e632794bd898d4446e5c3c13b276399a5afa1f

--- _next/static/chunks/da988b4a02da9b21.js
+++
@@ -1,8 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/43c19863fe36e93b.js",
-  "static/chunks/9b9bafd346f2e26c.js",
-  "static/chunks/0768df1ae6a31175.js",
-  "static/chunks/a55e453750c2a7fa.css",
-  "static/chunks/a61e516efd69d945.css",
-  "static/chunks/turbopack-51a1a7c9f83dca73.js"
-])
