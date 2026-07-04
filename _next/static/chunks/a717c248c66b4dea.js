dfc648449c3de3333834a776b10d8515ec981ab8
KrasimirKralev <KrasimirKralev@users.noreply.github.com>
2026-07-04 11:07:47 +0000

    deploy: 653ecabdd5361be81ea58188f49f5d9e68cbfe1a

---
+++ _next/static/chunks/a717c248c66b4dea.js
@@ -0,0 +1,8 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/7dca57e879216fea.js",
+  "static/chunks/3ae3d620010ff219.js",
+  "static/chunks/0768df1ae6a31175.js",
+  "static/chunks/d7f490333dcad795.css",
+  "static/chunks/4e7ae105ae746e08.css",
+  "static/chunks/turbopack-54b5547fb66477e8.js"
+])
---
+++ _next/static/lfktbgo1lY-b1tnuO_a0F/_buildManifest.js
@@ -0,0 +1,22 @@
+self.__BUILD_MANIFEST = {
+  "/404": [
+    "./static/chunks/4c86d7e7d6df834b.js"
+  ],
+  "/500": [
+    "./static/chunks/46c35f722acd61a1.js"
+  ],
+  "/_error": [
+    "./static/chunks/e9519b5810ef15b8.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/404",
+    "/500",
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
