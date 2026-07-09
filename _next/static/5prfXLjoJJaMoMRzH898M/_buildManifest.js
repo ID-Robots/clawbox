e530720d0ef9338d92c6411c4a19a6d0dd932c18
yalexx <yalexx@users.noreply.github.com>
2026-07-09 17:27:47 +0000

    deploy: 90c4fe1ace34dd38f8fb49417565470d5bd74d6d

---
+++ _next/static/5prfXLjoJJaMoMRzH898M/_buildManifest.js
@@ -0,0 +1,22 @@
+self.__BUILD_MANIFEST = {
+  "/404": [
+    "./static/chunks/d9f1f797695d7d9a.js"
+  ],
+  "/500": [
+    "./static/chunks/a6535af613b321b7.js"
+  ],
+  "/_error": [
+    "./static/chunks/e9519b5810ef15b8.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/404",
+    "/500",
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
