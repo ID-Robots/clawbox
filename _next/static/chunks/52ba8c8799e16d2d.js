2fe6bd595e976bf9425aeb33eb826e36c1334eea
KrasimirKralev <KrasimirKralev@users.noreply.github.com>
2026-08-08 17:21:00 +0000

    deploy: e34bed69c7f37312cb93c35c34168bd50ee5320d

---
+++ _next/static/chunks/52ba8c8799e16d2d.js
@@ -0,0 +1,8 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/b3e7f04921927c1a.js",
+  "static/chunks/b93312a88edc6c59.js",
+  "static/chunks/0768df1ae6a31175.js",
+  "static/chunks/9e529d850a10a6d3.css",
+  "static/chunks/dc1e9a5f4ee7caeb.css",
+  "static/chunks/turbopack-2116779f19663801.js"
+])
---
+++ _next/static/chunks/c7abfe2480c215eb.js
@@ -0,0 +1,8 @@
+__turbopack_load_page_chunks__("/404", [
+  "static/chunks/78da3526d0fa7b0b.js",
+  "static/chunks/0768df1ae6a31175.js",
+  "static/chunks/b93312a88edc6c59.js",
+  "static/chunks/e34c8a4269181a3f.js",
+  "static/chunks/b6b57afd46b61480.js",
+  "static/chunks/turbopack-574b66424c188ad7.js"
+])
