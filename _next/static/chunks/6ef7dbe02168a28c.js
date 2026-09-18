714084019c1ae01f2469c3048c1c09bb52a11d6d
KrasimirKralev <KrasimirKralev@users.noreply.github.com>
2026-09-18 16:44:27 +0000

    deploy: 238d97983e2119cbf3e47bd59288b5c60207d396

---
+++ _next/static/chunks/6ef7dbe02168a28c.js
@@ -0,0 +1,8 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/b3e7f04921927c1a.js",
+  "static/chunks/5da9c48811ef047a.js",
+  "static/chunks/a9ee6d16269089bb.js",
+  "static/chunks/d2af0f236ae64679.css",
+  "static/chunks/8ec82115c8ae24bd.css",
+  "static/chunks/turbopack-8fb87e02ba070ddb.js"
+])
